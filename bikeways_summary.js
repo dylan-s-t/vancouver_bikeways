628e4f29019cae33c55288b5ab2fce8886b61605
Dylan <dylantrerise@gmail.com>
2019-12-31 00:36:37 -0800

    added chart summarizing length of bike lanes per neighborhood

---
+++ bikeways_summary.js
@@ -0,0 +1,136 @@
+﻿bikeways_sum = {
+    "bikeways_split_stats": [
+        {
+            "OBJECTID": "1",
+            "name": "Arbutus-Ridge",
+            "FREQUENCY": "79",
+            "SUM_length_segment": "13143.23207"
+        },
+        {
+            "OBJECTID": "2",
+            "name": "Downtown",
+            "FREQUENCY": "206",
+            "SUM_length_segment": "34759.36165"
+        },
+        {
+            "OBJECTID": "3",
+            "name": "Dunbar-Southlands",
+            "FREQUENCY": "92",
+            "SUM_length_segment": "16453.92382"
+        },
+        {
+            "OBJECTID": "4",
+            "name": "Fairview",
+            "FREQUENCY": "78",
+            "SUM_length_segment": "16733.03544"
+        },
+        {
+            "OBJECTID": "5",
+            "name": "Grandview-Woodland",
+            "FREQUENCY": "46",
+            "SUM_length_segment": "9521.207262"
+        },
+        {
+            "OBJECTID": "6",
+            "name": "Hastings-Sunrise",
+            "FREQUENCY": "70",
+            "SUM_length_segment": "15802.5093"
+        },
+        {
+            "OBJECTID": "7",
+            "name": "Kensington-Cedar Cottage",
+            "FREQUENCY": "98",
+            "SUM_length_segment": "17509.25318"
+        },
+        {
+            "OBJECTID": "8",
+            "name": "Kerrisdale",
+            "FREQUENCY": "25",
+            "SUM_length_segment": "13985.54466"
+        },
+        {
+            "OBJECTID": "9",
+            "name": "Killarney",
+            "FREQUENCY": "57",
+            "SUM_length_segment": "16568.67043"
+        },
+        {
+            "OBJECTID": "10",
+            "name": "Kitsilano",
+            "FREQUENCY": "114",
+            "SUM_length_segment": "23934.10896"
+        },
+        {
+            "OBJECTID": "11",
+            "name": "Marpole",
+            "FREQUENCY": "109",
+            "SUM_length_segment": "18614.86037"
+        },
+        {
+            "OBJECTID": "12",
+            "name": "Mount Pleasant",
+            "FREQUENCY": "102",
+            "SUM_length_segment": "19732.17938"
+        },
+        {
+            "OBJECTID": "13",
+            "name": "Oakridge",
+            "FREQUENCY": "57",
+            "SUM_length_segment": "11086.00255"
+        },
+        {
+            "OBJECTID": "14",
+            "name": "Renfrew-Collingwood",
+            "FREQUENCY": "98",
+            "SUM_length_segment": "18008.5728"
+        },
+        {
+            "OBJECTID": "15",
+            "name": "Riley Park",
+            "FREQUENCY": "80",
+            "SUM_length_segment": "14331.73493"
+        },
+        {
+            "OBJECTID": "16",
+            "name": "Shaughnessy",
+            "FREQUENCY": "23",
+            "SUM_length_segment": "10316.13771"
+        },
+        {
+            "OBJECTID": "17",
+            "name": "South Cambie",
+            "FREQUENCY": "55",
+            "SUM_length_segment": "9127.582734"
+        },
+        {
+            "OBJECTID": "18",
+            "name": "Strathcona",
+            "FREQUENCY": "64",
+            "SUM_length_segment": "12324.09216"
+        },
+        {
+            "OBJECTID": "19",
+            "name": "Sunset",
+            "FREQUENCY": "135",
+            "SUM_length_segment": "17356.98628"
+        },
+        {
+            "OBJECTID": "20",
+            "name": "Victoria-Fraserview",
+            "FREQUENCY": "77",
+            "SUM_length_segment": "19133.20767"
+        },
+        {
+            "OBJECTID": "21",
+            "name": "West End",
+            "FREQUENCY": "104",
+            "SUM_length_segment": "14850.90208"
+        },
+        {
+            "OBJECTID": "22",
+            "name": "West Point Grey",
+            "FREQUENCY": "76",
+            "SUM_length_segment": "12938.45407"
+        }
+    ]
+}
